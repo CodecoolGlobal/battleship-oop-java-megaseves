3122aaade4ce3316012ff5da8fcdd4122ec604ca
agoston.fatsar <fatsar.agoston@gmail.com>
2022-08-10 20:55:44 +0200

    Add: Ship

--- src/game/Ship.java
+++ src/game/Ship.java
@@ -1,10 +1,30 @@
 package game;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class Ship {
 
     private List<Square> placeOfShip;
 
-    public Ship() {}
+    private final int length;
+
+    public Ship(ShipType type) {
+        this.length = type.length;
+    }
+
+    public int getLength() {return length;}
+
+    public void placeShip(Square[] placeOfShip) {
+        this.placeOfShip.addAll(Arrays.asList(placeOfShip));
+    }
+
+    public boolean hasSunk() {
+        for (Square square : placeOfShip) {
+            if (square.getSquareStatus() == SquareStatus.SHIP) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
