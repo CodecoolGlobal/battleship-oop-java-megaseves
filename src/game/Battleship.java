3169044c387d0fba2b7ad2d7ac32a8627e8c149b
agoston.fatsar <fatsar.agoston@gmail.com>
2022-08-10 10:15:57 +0200

    Add: printMenu

--- src/game/Battleship.java
+++ src/game/Battleship.java
@@ -5,6 +5,8 @@ public class Battleship {
     public static void main(String[] args) {
         Display display = new Display();
         Input input = new Input();
+        display.printMenu();
         Game game = new Game();
+
     }
 }
