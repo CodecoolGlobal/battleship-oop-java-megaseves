3909b6bbdf16b312396802accd1a9f59e37ff067
agoston.fatsar <fatsar.agoston@gmail.com>
2022-08-10 11:48:19 +0200

    Add: Enums

--- src/game/Square.java
+++ src/game/Square.java
@@ -7,4 +7,9 @@ public class Square {
     private int yCoordinate;
 
     private String squareStatus;
+
+    public Square() {
+        this.squareStatus = SquareStatus.OCEAN.character;
+    }
+
 }
