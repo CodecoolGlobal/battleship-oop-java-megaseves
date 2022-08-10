7d4ad55bb254597d598500030f66c45d08887422
M9Bene <bene9marci@gmail.com>
2022-08-10 14:06:29 +0200

    Add: started board's checkIfValid method

--- src/game/Board.java
+++ src/game/Board.java
@@ -4,5 +4,25 @@ public class Board {
 
     private Square[][] ocean;
 
+    public Board() {
+        ocean = new Square[10][10];
+        for (int row = 0; row < 10; row++) {
+            for (int col = 0; col < 10; col++) {
+                ocean[row][col] = new Square(row, col);
+            }
+        }
+    }
+
+    public Square[][] getOcean() {
+        return ocean;
+    }
+
+
+
+    public boolean checkIfValid(int x, int y){
+        return ocean[x][y].getSquareStatus() != SquareStatus.SHIP;
+
+    }
+
     public boolean isPlacementOk(int[] coordinates) {return true;}
 }
