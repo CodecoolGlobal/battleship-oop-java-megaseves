b5b31c1a7c7c5aaf11ba8a602415ca1843ba0d30
Andras Csovari <csovaria@gmail.com>
2022-08-10 11:52:03 +0200

    Add: getShipPlacement method, that gets user input and validates the format

--- src/game/Battleship.java
+++ src/game/Battleship.java
@@ -1,5 +1,7 @@
 package game;
 
+import java.util.Arrays;
+
 public class Battleship {
 
     public static void main(String[] args) {
--- src/game/Input.java
+++ src/game/Input.java
@@ -3,6 +3,8 @@ package game;
 import java.util.Scanner;
 
 public class Input {
+    private final String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
 
     public int getInputforMenu() {
         while (true) {
@@ -14,7 +16,24 @@ public class Input {
         }
     }
 
-
+    public int[] getShipPlacement(){
+        while(true){
+            try{
+                Scanner scanner = new Scanner(System.in);
+                String input = scanner.nextLine();
+                char firstPartInput = Character.toUpperCase(input.charAt(0));
+                int secondPartInput = Integer.parseInt(input.substring(1));
+                int firstConvertedCoord = alphabet.indexOf(firstPartInput);
+                int secondConvertedCoord = secondPartInput - 1;
+                if (firstConvertedCoord < 10 && firstConvertedCoord > 0 && secondConvertedCoord < 10){ //Todo make it dynamic, add table size
+                    return new int[]{firstConvertedCoord, secondConvertedCoord};
+                }
+                System.out.println("Invalid input"); //Todo remove print from here
+            } catch (Exception ignored){
+                System.out.println("Invalid input"); //Todo remove print from here
+            }
+        }
+    }
 
     private boolean validateInput() {return true;}
 }
